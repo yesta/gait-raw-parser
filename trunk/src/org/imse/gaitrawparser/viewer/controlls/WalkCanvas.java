72b14f6d1f679866e933e9839be9b5925bfad39b
matej.svejda <matej.svejda@aa68de6e-1f09-11df-8ebe-b16711b242dd>
2010-04-23 11:12:54 +0000

    Time-Slider for replay

--- trunk/src/org/imse/gaitrawparser/viewer/controlls/WalkCanvas.java
+++ trunk/src/org/imse/gaitrawparser/viewer/controlls/WalkCanvas.java
@@ -1,12 +1,14 @@
-package org.imse.gaitrawparser.viewer;
+package org.imse.gaitrawparser.viewer.controlls;
 
 import java.util.List;
 
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.events.PaintEvent;
 import org.eclipse.swt.events.PaintListener;
+import org.eclipse.swt.graphics.Color;
+import org.eclipse.swt.graphics.Device;
 import org.eclipse.swt.graphics.GC;
-import org.eclipse.swt.graphics.Rectangle;
+import org.eclipse.swt.graphics.Point;
 import org.eclipse.swt.widgets.Canvas;
 import org.eclipse.swt.widgets.Composite;
 import org.imse.gaitrawparser.data.PressurePoint;
@@ -17,6 +19,8 @@ public class WalkCanvas extends Canvas implements PaintListener {
 	private int width;
 	private int height;
 	private int scale = 3;
+	private Color[] colors;
+	private int stepsCount = 0;
 
 	public WalkCanvas(Composite parent, int style) {
 		super(parent, style);
@@ -44,8 +48,8 @@ public class WalkCanvas extends Canvas implements PaintListener {
 		return width;
 	}
 	
-	public void setDimensionsFromPressurePoints() {
-		if (points != null) {
+	public Point calculateDimensionsFromPressurePoints() {
+		if (points == null) {
 			throw new RuntimeException("Pressurepoints have to be set first.");
 		}
 		height = 0;
@@ -58,6 +62,8 @@ public class WalkCanvas extends Canvas implements PaintListener {
 				height = p.getY();
 			}
 		}
+		Point result = new Point(width, height);
+		return result;
 	}
 
 	public void paintControl(PaintEvent e) {
@@ -65,10 +71,36 @@ public class WalkCanvas extends Canvas implements PaintListener {
 		if (points == null) {
 			return;
 		}
-		gc.setBackground(e.display.getSystemColor(SWT.COLOR_BLACK)); 
+		int i = 1;
 		for (PressurePoint p : points) {
-			gc.fillRectangle(p.getX() * scale, p.getY() * scale, scale, scale);
+			if (i > stepsCount) {
+				return;
+			}
+			i++;
+			gc.setBackground(getColorForPressure(p.getPressure(), e.display));
+			gc.fillRectangle(p.getX() * (scale + 1), p.getY() * (scale + 1), scale, scale);
 		}
+	}
+	
+	private Color getColorForPressure(int pressure, Device device) {
+		if (colors == null) {
+			colors = new Color[8];
+			colors[0] = new Color(device, 181, 213, 254);
+			colors[1] = new Color(device, 210, 157, 62);
+			colors[2] = new Color(device, 65, 154, 72);
+			colors[3] = new Color(device, 233, 0, 40);
+			colors[4] = new Color(device, 3, 78, 145);
+			colors[5] = new Color(device, 113, 60, 23);
+			colors[6] = new Color(device, 0, 82, 39);
+			colors[7] = new Color(device, 0, 0, 0);
+		}
+		return colors[pressure];
+	}
+	
+	public void setStepsCount(int count) {
+		this.stepsCount  = count;
+		
+		redraw();
 	}
 
 }
