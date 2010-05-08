660d058eb2659f7233eef45a759e62d508bee707
jenny.reinelt <jenny.reinelt@aa68de6e-1f09-11df-8ebe-b16711b242dd>
2010-05-08 21:57:56 +0000

    added outerLine

--- src/org/imse/gaitrawparser/data/LeftFootPrint.java
+++ src/org/imse/gaitrawparser/data/LeftFootPrint.java
@@ -1,7 +1,5 @@
 package org.imse.gaitrawparser.data;
 
-import java.util.ArrayList;
-import java.util.List;
 
 import org.eclipse.swt.graphics.Point;
 import org.imse.gaitrawparser.data.PressurePoint.Foot;
@@ -28,6 +26,7 @@ public class LeftFootPrint extends FootPrint {
 
 	@Override
 	protected boolean isInnerPoint(int x, int y) {
+	    //TODO y = lenY - 1
 		if (y + 1 < lenY && pixel[x][y + 1] == false) {
 			return true;
 		} else {
@@ -36,18 +35,53 @@ public class LeftFootPrint extends FootPrint {
 	}
 
 	@Override
-	protected double getTargetAngle() {
+	protected double getTargetAngleAG() {
 		return -0.1;
 	}
 
 	@Override
-	protected boolean isOutside(double py, double yl, double yr) {
+	protected boolean isOutsideInnerLine(double py, double yl, double yr) {
 		if (yl < py + 1 || yr < py + 1) {
 			return false;
 		} else {
 			return true;
 		}
 	}
+
+    @Override
+    protected boolean isOuterPoint(int x, int y){
+        if(y == 0){
+            return true;
+        }
+        else if (y - 1 >= 0 && pixel[x][y - 1] == false) {
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    protected Line getOuterLine(Point p1, Point p2) {
+        if(p1.y > p2.y){
+            return new Line(p1.x, p1.y, p2.x, p2.y);
+        } else {
+            return new Line(p1.x + 1, p1.y, p2.x + 1, p2.y);
+        }
+    }
+
+    @Override
+    protected double getTargetAngleLR() {
+        return -0.1;
+    }
+
+    @Override
+    protected boolean isOutsideOuterLine(double py, double yl, double yr){
+        if(py < yl || py < yr){
+           return false; 
+        } else {
+            return true;
+        }
+       
+    }
 
 	
 
