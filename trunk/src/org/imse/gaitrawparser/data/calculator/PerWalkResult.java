0ad49042b775c5f49d950da6e4e0334e2bb2174d
jenny.reinelt <jenny.reinelt@aa68de6e-1f09-11df-8ebe-b16711b242dd>
2010-06-10 10:12:29 +0000

    PerWalkResult

---
+++ trunk/src/org/imse/gaitrawparser/data/calculator/PerWalkResult.java
@@ -0,0 +1,9 @@
+package org.imse.gaitrawparser.data.calculator;
+
+public class PerWalkResult extends MetricResult {
+
+    public PerWalkResult(String name, String description) {
+        super(name, description);
+    }
+
+}
