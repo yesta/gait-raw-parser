ac4568cd0909b868db6e738db287a5a53f980fbe
jenny.reinelt <jenny.reinelt@aa68de6e-1f09-11df-8ebe-b16711b242dd>
2010-06-10 10:40:40 +0000

    PerWalkResult

--- src/org/imse/gaitrawparser/data/calculator/PerWalkResult.java
+++ src/org/imse/gaitrawparser/data/calculator/PerWalkResult.java
@@ -2,8 +2,21 @@ package org.imse.gaitrawparser.data.calculator;
 
 public class PerWalkResult extends MetricResult {
 
+    private double value;
+    
     public PerWalkResult(String name, String description) {
         super(name, description);
     }
-
+    
+    public void setValue(double value) {
+        this.value = value;
+    }
+    
+    public double getValue() {
+        return value;
+    }
+    
+    public String toString() {
+        return "Caculator: " + getName() + ", Description: " + getDescription() + "\n" + "Value per Walk: " + value;
+    }
 }
